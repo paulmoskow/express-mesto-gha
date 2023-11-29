ed06803c1e7a3fdf0cc83a5db789d321911ac04d
Pavel Moskov <pavel.moskov@gmail.com>
2023-11-29 23:36:58 +0200

    13 sprint

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,24 +15,50 @@ module.exports.createCard = (req, res) => {
 
   Card.create({ name, link })
     .then(card => res.send({ data: card }))
-    .catch(err => res.status(400).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' })
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' })
+      }
+    });
 };
 /////////////////////////////////////////////////
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .then(card => res.send({ data: card }))
-    .catch(() => res.status(404).send({ message: 'Произошла ошибка' }));
-};
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Карточка не найдена' })
+      }
+    });
+  };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-  .then(card => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-};
+    .then(card => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Карточка не найдена' })
+      } if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' })
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' })
+      }
+    });
+  };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-  .then(card => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-};
+    .then(card => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Карточка не найдена' })
+      } if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка' })
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' })
+      }
+    });
+  };
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
 const User = require('../models/User');
 
+
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then(users => res.send({ data: users }))
-    .catch(() => res.status(404).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 
@@ -13,28 +14,56 @@ module.exports.createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
-    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' })
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' })
+      }
+    });
 };
 /////////////////////////////////////////////////
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
     .then(user => res.send({ data: user }))
-    .catch(() => res.status(404).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Пользователь не найден' })
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' })
+      }
+    });
 };
 
 module.exports.updateUserProfile = (req, res) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.params.id, { name: name, about: about }, { new: true })
-  .then(user => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then(user => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Пользователь не найден' })
+      } if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' })
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' })
+      }
+    });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.params.id, { avatar: avatar }, { new: true })
-  .then(user => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then(user => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'Пользователь не найден' })
+      } if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' })
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' })
+      }
+    });
 };
